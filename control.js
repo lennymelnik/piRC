d7304c1f8f64935207b154ca775a3e8ce68a0de0
Leonard Melnik <leonardmelnik@gmail.com>
2021-12-07 19:49:00 -0500

    Will now test

---
+++ control.js
@@ -0,0 +1,18 @@
+var piblaster = require('pi-blaster.js');
+
+
+
+const express = require('express');const app = express();const http = require('http');const server = http.createServer(app);const { Server } = require("socket.io");const io = new Server(server);
+app.get('/', (req, res) => {  res.sendFile(__dirname + '/index.html');});
+io.on('connection', (socket) => {  console.log('a user connected');
+
+
+    socket.on('modify', (value) => {    piblaster.setPwm(18, value ); 
+        console.log("Modify")
+    })
+
+});
+
+
+
+server.listen(3000, () => {  console.log('listening on *:3000');});
