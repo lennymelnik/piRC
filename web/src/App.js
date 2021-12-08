6e8deff9f75adc65c9650104fa79eecf627deb5e
Leonard Melnik <leonardmelnik@gmail.com>
2021-12-07 20:46:05 -0500

    Anyone can control now

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(`http://192.168.0.129:3001`);
+    const newSocket = io(`http://108.46.212.16:3001`);
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label for="customRange1" class="form-label">Example range</label>
+        <label for="customRange1" class="form-label">Move Servo</label>
 <input min='.075' max = ".25" step ="0.005" type="range" class="form-range" id="customRange1" onChange={(e)=>{  console.log("updating");  socket.emit('modify', e.target.value);}}/>
 
 
