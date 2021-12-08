5cefa5d532d9c1696d6595adb11b6430a5aba8b7
Leonard Melnik <leonardmelnik@gmail.com>
2021-12-07 21:04:20 -0500

    Switched to domain

--- control.js
+++ control.js
@@ -7,7 +7,7 @@ const express = require('express');const app = express();
 app.use(cors())
 const http = require('http');const server = http.createServer(app);
 const io = require("socket.io")(server,{
-    cors: {    origin: "http://108.46.212.16" }
+    cors: {    origin: "http://rc.hackerslab.ml" }
 });
 app.get('/', (req, res) => {  res.sendFile(__dirname + '/index.html');});
 io.on('connection', (socket) => {  console.log('a user connected');
